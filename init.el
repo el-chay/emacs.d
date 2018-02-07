5a18127bbde4f52af752097f85583f168921fe04
Antonio Chay <achay@pindrop.com>
2018-02-07 18:09:15 -0500

    Set default font (Inconsolata, H140)

--- init.el
+++ init.el
@@ -55,5 +55,12 @@
 
 (use-package flycheck
   :ensure t
-  :init (global-flycheck-mode))
+  :init (global-flycheck-mode)
+  :config (setq flycheck-shellcheck-follow-sources nil))
 ;;; init.el ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "PfEd" :family "Inconsolata")))))
